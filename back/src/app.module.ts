73c09da52091e5455feb383c459d524aa4198e72
nshadybekov <nshadybekov@adsterra.com>
2022-05-14 14:19:03 +0300

    docker database

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LinksModule } from './links/links.module';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as Joi from '@hapi/joi';
 
 import configuration from '../config/configuration';
 
@@ -13,16 +14,19 @@ import configuration from '../config/configuration';
     ConfigModule.forRoot({
       load: [configuration],
     }),
-    TypeOrmModule.forRoot({
-      url: process.env.DATABASE_URL,
-      type: 'postgres',
-      ssl: false,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
     }),
     LinksModule,
     UsersModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
